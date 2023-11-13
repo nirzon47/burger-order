1a6f1d8d5640bfed3265c04deb5718d87bcb2d3b
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-13 14:23:36 +0530

    UI v0

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['*.html'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				menu: 'url("../assets/menu.jpg")',
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+}
